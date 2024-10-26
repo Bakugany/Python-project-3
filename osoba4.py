6c657723c9dc82a4bd2da13fe7ea108bd0938305
BartekRozenberg <bartekrozenberg@gmail.com>
2024-10-26 12:06:45 +0200

    Update osoba4.py
    Drobne zmiany

--- osoba4.py
+++ osoba4.py
@@ -2,19 +2,18 @@ import json
 import random
 
 ### Zapisywanie danych do pliku json ###
-def zapisz_dane(nazwa_pliku):
+def zapisz_dane_json(nazwa_pliku):
     dane = {
         "Model": random.choice(['A', 'B', 'C']),
         "Wynik": random.randrange(0, 1000),
         "Czas":  str(random.randrange(0, 1000)) + 's'
     }
-
     with open(nazwa_pliku, 'w') as plik:
         json.dump(dane, plik)
 
 
 ### Odczytanie danych z pliku json ###
-def odczytaj_dane(nazwa_pliku):
+def odczytaj_dane_json(nazwa_pliku):
     with open(nazwa_pliku, 'r') as plik:
         dane = json.load(plik)
         if(dane and dane['Model'] == 'A'):
