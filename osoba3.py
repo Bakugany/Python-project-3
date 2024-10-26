488b935a3337264f481c5c79f2082a239a5b4ba2
JKLRak <jacekkarolrak2@gmail.com>
2024-10-26 05:45:03 -0400

    Update osoba3.py

--- osoba3.py
+++ osoba3.py
@@ -1,18 +1,30 @@
 import csv;
 import random;
 
-filename = "Dane.csv"
+#filename jest .csv
+def write_file(filename):
 
-plik = open(filename, 'w')
-writer = csv.writer(plik, delimiter=';')
+    plik = open(filename, 'w')
+    writer = csv.writer(plik, delimiter=';')
 
-header = ["Model", "Wynik", "Czas"]
-writer.writerow(header);
+    header = ["Model", "Wynik", "Czas"]
+    writer.writerow(header);
 
-Model = random.choice(["A","B","C"])
-Wynik = random.randint(0, 1000)
-Czas = str(random.randint(0,1000)) + "s"
-info = [Model, Wynik, Czas]
-writer.writerow(info);
+    Model = random.choice(["A","B","C"])
+    Wynik = random.randint(0, 1000)
+    Czas = str(random.randint(0,1000)) + "s"
+    info = [Model, Wynik, Czas]
+    writer.writerow(info);
 
-plik.close()
+    plik.close()
+
+#filename jest .csv
+def reading(filename)-> int:
+    with open(filename, 'r') as plik:
+        czytelnik = csv.reader(plik, delimiter=';')
+        next(czytelnik)
+        wiersz = next(czytelnik)
+        if(wiersz and wiersz[0] == "A"):
+            trimmed_text = wiersz[2][:-1]
+            return int(trimmed_text)
+        return 0;
