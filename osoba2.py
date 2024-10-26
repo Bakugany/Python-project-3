44eaab785f87c94861ae56f85694d5262b957f90
zelbetowy_samuraj <164026120+sapankowski@users.noreply.github.com>
2024-10-26 13:01:57 +0200

    Update osoba2.py

--- osoba2.py
+++ osoba2.py
@@ -4,32 +4,36 @@ import osoba3 as o3
 import osoba4 as o4
 
 # miesiace = [.], dni = [[.], [.]], pory = [.], tryb = 'r'/'w', rozszerzenie = 'csv'/'json',
-def przetwarzanie_pliku(miesiace, dni, pory, tryb, rozszerzenie):
+def przetwarzanie_plikow(miesiace, dni, pory, tryb, rozszerzenie):
     cnt = 0
     for i in range(len(miesiace)):
         for j in range(len(dni[i])):
             sciezka = os.path.join(os.getcwd(), miesiace[i], dni[i][j])
             sciezka = Path(sciezka)
+            
             if len(pory) > cnt:
                 if pory[cnt] == 'r':
                     sciezka = sciezka / 'rano'
                 elif pory[cnt] == 'w':
                     sciezka = sciezka / 'wieczor'
+                    
             else:
                 sciezka = sciezka / 'rano'
+                
             cnt += 1
+            
             if not os.path.exists(sciezka):
                 os.makedirs(sciezka)
+                
             if rozszerzenie == 'json':
                 sciezka = sciezka / 'Dane.json'
-                print(sciezka)
                 if tryb == 'r':
                     o4.odczytaj_dane_json(sciezka)
                 elif tryb == 'w':
                     o4.zapisz_dane_json(sciezka)
+                    
             elif rozszerzenie == 'csv':
                 sciezka = sciezka / 'Dane.csv'
-                print(sciezka)
                 if tryb == 'r':
                     o3.odczytaj_dane_csv(sciezka)
                 elif tryb == 'w':
